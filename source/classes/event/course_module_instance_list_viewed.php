c9a5210217562fbcf22c0c3edad7672c5b0e62c4
Edwin <ne.edwin96@gmail.com>
2019-02-07 17:40:04 +0100

    Add template

---
+++ source/classes/event/course_module_instance_list_viewed.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * The mod_testmodule instance list viewed event.
+ *
+ * @package    mod_testmodule
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_testmodule\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * The mod_testmodule instance list viewed event class.
+ *
+ * @package    mod_testmodule
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class course_module_instance_list_viewed extends \core\event\course_module_instance_list_viewed {
+}
