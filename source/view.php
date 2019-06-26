cfb6ed58a11e71eb8b9c8fabb49f7cefae7f8e6a
EdwinNe <ne.edwin96@gmail.com>
2019-06-26 17:20:48 +0200

    Added REST-Webservice to remotely create an envelope

--- source/view.php
+++ source/view.php
@@ -6,8 +6,6 @@ require_once(__DIR__ . '/locallib.php');
 
 include(__DIR__ . '/view_init.php');
 
-global $SESSION;
-
 echo $OUTPUT->heading('Start');
 
 global $SESSION;
@@ -17,7 +15,7 @@ $password = "qwert12345";
 $integratorKey = "3d677c95-6e5e-4133-9bd6-5670a4db865c"; //To identify third-party Apps
 
 $recipient_email = 'nebenmail09@gmail.com';
-$name = 'Edwin Neumann';
+$name = 'Edwin';
 $document_name = 'Bachelorthesis.pdf';
 
 // construct the authentication header:
@@ -45,85 +43,12 @@ $accountId = $response["loginAccounts"][0]["accountId"];
 $baseUrl = $response["loginAccounts"][0]["baseUrl"];
 curl_close($curl);
 
-/////////////////////////////////////////////////////////////////////////////////////////////////
-// STEP 2 - Create an envelope with one recipient, one tab, one document and send!
-/////////////////////////////////////////////////////////////////////////////////////////////////
-
-$file_contents = file_get_contents(__DIR__ . '/resources/Bachelorthesis.pdf');
-$base64Data = base64_encode($file_contents);
-
-$data = array("accountId" => $accountId,
-        "emailSubject" => "Please sign this document.",
-        "documents" => array(
-                array(
-                        "documentId" => "1",
-                        "name" => $document_name,
-                        "documentBase64" => $base64Data,
-                )),
-        "recipients" => array (
-                "signers" => array(
-                        array(
-                                "email" => $recipient_email,
-                                "name" => $name,
-                                "recipientId" => "1",
-                                "clientUserId" => "1",
-                                "tabs" => array (
-                                        "signHereTabs" => array (
-                                                array (
-                                                        "xPosition" => "394",
-                                                        "yPosition" => "187",
-                                                        "documentId" => "1",
-                                                        "recipientId" => "1",
-                                                        "pageNumber" => "3"
-                                                )
-                                        ),
-                                        "dateSignedTabs" => array (
-                                                array (
-                                                        "xPosition" => "119",
-                                                        "yPosition" => "220",
-                                                        "documentId" => "1",
-                                                        "fontSize" => "Size16",
-                                                        "recipientId" => "1",
-                                                        "pageNumber" => "3",
-                                                )
-                                        )
-                                )
-                        )
-                )
-        ),
-        "status" => "sent",
-);
-
-$data_string = json_encode($data);
-
-// *** append "/envelopes" to baseUrl and as signature request endpoint
-$curl = curl_init($baseUrl . "/envelopes" );
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-                'Content-Type: application/json',
-                'Content-Length: ' . strlen($data_string),
-                "X-DocuSign-Authentication: $header" )
-);
-
-$json_response = curl_exec($curl);
-$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-if ( $status != 201 ) {
-    echo "error calling webservice, status is:" . $status . "\nerror text is --> ";
-    print_r($json_response); echo "\n";
-    exit(-1);
-}
-
-$response = json_decode($json_response, true);
-$envelopeId = $response["envelopeId"];
-curl_close($curl);
-
 // 3: Get the URL of the Embedded Singing View
+$envelopeId = $_GET['envelopeId'];
 
 $data = array("returnUrl" => "http://www.docusign.com/devcenter",
-        "authenticationMethod" => "None", "email" => $recipient_email,
-        "userName" => $name, "recipientId" => "1", "clientUserId" => "1"
+        "authenticationMethod" => "None", "email" => 'nebenmail09@gmail.com',
+        "userName" => 'Edwin', "recipientId" => "1", "clientUserId" => "1"
 );
 
 $data_string = json_encode($data);
@@ -148,7 +73,7 @@ if ( $status != 201 ) {
 $response = json_decode($json_response, true);
 $url = $response["url"];
 
-echo '<a href="'.$url.'">Click this link to get to the signing-view</a>';
+echo '<iframe width="700" height="500" src="' . $url . '">/iframe>';
 
 // Finish the page.
 echo $OUTPUT->footer();
